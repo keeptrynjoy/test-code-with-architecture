a241902c63fbdec7bbbfa44081eecaefc3f1bc11
sm.kim2@okestro.com <sm.kim2@okestro.com>
2024-06-16 20:38:35 +0900

    MailSender 의존 역전 적용

---
+++ src/main/java/com/example/demo/user/infrastructure/MailSenderImpl.java
@@ -0,0 +1,23 @@
+package com.example.demo.user.infrastructure;
+
+import com.example.demo.user.service.port.MailSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MailSenderImpl implements MailSender {
+
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void send(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        javaMailSender.send(message);
+    }
+}
